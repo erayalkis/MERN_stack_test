85b8fa3db79b2818848070d2a103945e20f0269b
Lily <lolpo12345@outlook.com>
2022-04-10 00:55:30 +0300

    Fix invaild access for request parameters

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,15 +5,15 @@ const router = express.Router();
 
 router.get("/test", (req, res) => res.send("This is the users route!"));
 router.get("/register", (req, res) => {
-  User.findOne({email: req.email})
+  User.findOne({email: req.body.email})
   .then(user => {
     if(user) {
       res.status(400).json({email: "A user with this email already exists!"});
     } else {
       const newUser = new User({
-        handle: req.handle,
-        email: req.email,
-        password: req.password
+        handle: req.body.handle,
+        email: req.body.email,
+        password: req.body.password
       });
 
       bcrypt.genSalt(10, (err, salt) => {

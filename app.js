5283795c9b078f9f52e851d63f9b90ed6da0bf9e
Lily <lolpo12345@outlook.com>
2022-04-09 22:30:05 +0300

    Add listen call on port 5000

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const app = express();
 
 const PORT = process.env.port || 5000;
 
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
 app.get("/", (req, res) => res.send("Hello, World!"));

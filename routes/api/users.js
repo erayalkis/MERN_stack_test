e7139c8931b29232cfdfda9bca73fd8fa6776781
Lily <lolpo12345@outlook.com>
2022-04-10 01:36:55 +0300

    Import jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
-const bcrypt = require("bcryptjs");
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
 const router = express.Router();
 

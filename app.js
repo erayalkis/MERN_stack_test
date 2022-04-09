d9d0e0c6963fbb1e516008d542ab047a6479b240
Lily <lolpo12345@outlook.com>
2022-04-09 23:07:32 +0300

    Create connection to MongoDB

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const app = express();
 
 const PORT = process.env.port || 5000;
 
+try {
+  mongoose.connect(dbURI, { useNewUrlParser: true }).then(() => console.log("Connected to MongoDB successfully"))
+} catch(e) {
+  console.log("An error occured during connection to MongoDB:");
+  console.log(e);
+}
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 app.get("/", (req, res) => res.send("Hello, World!"));

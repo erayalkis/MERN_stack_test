57f7ba06e7ba24dca9eb0a12d48cd5d559409a33
Lily <lolpo12345@outlook.com>
2022-04-09 23:52:47 +0300

    Add code to tweets router

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/test", (req, res) => res.send("This is a users route!"));
+router.get("/test", (req, res) => res.send("This is the users route!"));
 
 module.exports = router;

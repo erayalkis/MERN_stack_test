f4b183f4ddf47b301384b3d04ff24374a6777671
Lily <lolpo12345@outlook.com>
2022-04-10 00:45:31 +0300

    Create code for user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,33 @@
+const bcrypt = require("bcryptjs");
 const express = require("express");
+const User = require("../../models/User");
 const router = express.Router();
 
 router.get("/test", (req, res) => res.send("This is the users route!"));
+router.get("/register", (req, res) => {
+  User.findOne({email: req.email})
+  .then(user => {
+    if(user) {
+      res.status(400).json("A user with this email already exists!");
+    } else {
+      const newUser = new User({
+        handle: req.handle,
+        email: req.email,
+        password: req.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if(err) throw err;
+
+          newUser.password = hash;
+          newUser.save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err))
+        })
+      })
+    }
+  })
+});
 
 module.exports = router;

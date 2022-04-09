ed8649168238e2f27566322dfdbfe2f8ceb6d025
Lily <lolpo12345@outlook.com>
2022-04-09 23:58:56 +0300

    Use routes for users and tweets

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dbURI = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");ü
+const tweets = require("./routes/api/tweets");
 const app = express();
 
 const PORT = process.env.port || 5000;
@@ -15,3 +17,5 @@ try {
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 app.get("/", (req, res) => res.send("Hello, World!"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);

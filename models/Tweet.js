1ca4995879b8d4f1c17ff9831ec40f7ded5b240e
Lily <lolpo12345@outlook.com>
2022-04-10 03:25:37 +0300

    Create Tweet schema

---
+++ models/Tweet.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TweetSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  }
+}, {timestamps: true})
+
+module.exports = Tweet = mongoose.model('Tweet', TweetSchema);

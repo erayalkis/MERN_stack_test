294dca227c779379ee6fa185dc974c479c731b35
Lily <lolpo12345@outlook.com>
2022-04-11 17:01:45 +0300

    Update session_api_utils

--- client/util/session_api_util.js
+++ client/util/session_api_util.js
@@ -1,7 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const setAuthToken = token => {
-  if (!token) delete axios.defaults.headers.common['Authorization'];
+export const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common["Authorization"] = token;
+  } else {
+    delete axios.defaults.headers.common["Authorization"];
+  }
+};
 
-  axios.defaults.headers.common['Authorization'] = token;
+export const signup = (userData) => {
+  return axios.post("/api/users/register", userData);
+};
+
+export const login = (userData) => {
+  return axios.post("/api/users/login", userData);
 };
